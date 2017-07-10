27f66f3830823d465d74a74b16c79e55a656a4e4
Miguel Monje <monje.miguel4@gmail.com>
2017-07-10 13:02:54 +0200

    Segunda Versión

--- src/main/java/es/cic/taller/ejercicio06/MyUI.java
+++ src/main/java/es/cic/taller/ejercicio06/MyUI.java
@@ -1,15 +1,17 @@
 package es.cic.taller.ejercicio06;
 
+
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
+
+import com.vaadin.ui.Component;
+import com.vaadin.ui.GridLayout;
+
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -22,19 +24,25 @@ import com.vaadin.ui.VerticalLayout;
  */
 @Theme("mytheme")
 public class MyUI extends UI {
-	private TapeteForm tapete1 = new TapeteForm(this);
-	private TapeteForm tapete2 = new TapeteForm(this);
-	private TapeteForm tapete3 = new TapeteForm(this);
-	private TapeteForm tapete4 = new TapeteForm(this);
+	private TapeteForm tapete1 = new TapeteForm(this,1);
+	private TapeteForm tapete2 = new TapeteForm(this,2);
+	private TapeteForm tapete3 = new TapeteForm(this,3);
+	private TapeteForm tapete4 = new TapeteForm(this,4);
+	 private GridLayout mesa;
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final HorizontalLayout layout = new HorizontalLayout();
+    	
+    	final VerticalLayout layout = new VerticalLayout();
+    	
+       Component[] tapetes = {tapete1,tapete2,tapete3,tapete4};
         
-       
-       
-
-        layout.addComponents(tapete1,tapete2,tapete3,tapete4);
         
+        mesa = new GridLayout();
+        mesa.addStyleName("outlined");
+        mesa.setSpacing(true);
+        mesa.setSizeFull();
+        generateMatrixGrid(4, 2,tapetes);
+        layout.addComponents(mesa);
         setContent(layout);
     }
     
@@ -42,5 +50,22 @@ public class MyUI extends UI {
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
+    }
+    private void generateMatrixGrid(final int rows, final int columns,Component[] tapetes) {
+        mesa.removeAllComponents();
+        mesa.setRows(rows);
+        mesa.setColumns(columns);
+        int i=0;
+        for (int row = 0; row < mesa.getRows(); row++) {
+            for (int col = 0; col < mesa.getColumns(); col++) {
+                if (col==0) {
+                	Component tapete = tapetes[i];
+                	mesa.addComponent(tapete);
+                	i++;
+                                }
+                mesa.setRowExpandRatio(row, 9.0f);
+                mesa.setColumnExpandRatio(col, 4.0f);
+            }
+        }
     }
 }
--- src/main/java/es/cic/taller/ejercicio06/TapeteForm.java
+++ src/main/java/es/cic/taller/ejercicio06/TapeteForm.java
@@ -1,30 +1,48 @@
 package es.cic.taller.ejercicio06;
 
 import com.vaadin.data.Binder;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 public class TapeteForm extends FormLayout{
-	 private VerticalLayout layout = new VerticalLayout();
+	 	private VerticalLayout layout = new VerticalLayout();
+	 	Baraja baraja=new Baraja();
+	    private TextField carta1 = new TextField("Carta 1:");
 	    
-	    private TextField carta1 = new TextField();
+	    private TextField carta2 = new TextField("Carta 2:");
 	    
-	    private TextField carta2 = new TextField();
+	    private TextField carta3 = new TextField("Carta 3:");
 	    
-	    private TextField carta3 = new TextField();
-	    
-	    private TextField carta4 = new TextField();
-
-	    private Binder<Tapete> binder = new Binder<>(Tapete.class);
+	    private TextField carta4 = new TextField("Carta 4:");
+	    private Label jugador = new Label();
+	    private Button boton=new Button("MUS");
 	    
 	    private MyUI myUI;
 	    
-	    public TapeteForm(MyUI myUI) {
+	    public TapeteForm(MyUI myUI,int num) {
 	        this.myUI = myUI;
-	        addComponents(carta1,carta2,carta3,carta4);
-	
+	        jugador.setCaption("jugador "+num);
+	        addComponents(jugador,carta1,carta2,carta3,carta4);
+	        carta1.setValue(asignaMano());
+	        carta2.setValue(asignaMano());
+	        carta3.setValue(asignaMano());
+	        carta4.setValue(asignaMano());
+	        addComponents(carta1,carta2,carta3,carta4, boton);
+	        boton.addClickListener( e -> {
+	        	carta1.setValue(asignaMano());
+	            carta2.setValue(asignaMano());
+	            carta3.setValue(asignaMano());
+	            carta4.setValue(asignaMano());
+	});
 	    }
+	    public String asignaMano() {
+	    	int num=(int) (Math.random()*40);
+	    	return (baraja.getCarta(num).getNumero()+" "+baraja.getCarta(num).getPalo());
+	    	
+	}
 	    
 	 
 	    }
